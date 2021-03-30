9ccf72cf79a79889bac70a0ad05a0b4dd74f5e4d
zhangyuanes <zhangyuanes@gmail.com>
2021-03-30 10:23:37 +0800

    update readme and test code

---
+++ ex1/helloworld.c
@@ -0,0 +1,26 @@
+/**
+ * @file helloworld.c
+ * @author zhangyuanes (zhangyuanes@gmail.com)
+ * @brief 测试程序
+ * @version 0.1
+ * @date 2021-03-30
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello World to Operating System");
+    return 0;
+}
+
+/**
+ * @brief 编译调试
+ * 
+ * gcc helloworld.c -o helloworld
+ * ./helloworld
+ * 
+ */
